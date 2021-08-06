2372203c04e3ce88ffea5f1255031253abc42367
Shaalan Marwan <salsanjary@gmail.com>
2021-08-07 01:10:00 +0300

    ✨ add meta tag for admin page

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,3 +1,11 @@
+import Head from "next/head";
+import React from "react";
+import MetaTags from "../../components/Metatags";
 export default function AdminPostsPage({}) {
-  return <main></main>;
+  return (
+    <main>
+      <MetaTags title="admin page" />
+      <h1>Edit post</h1>
+    </main>
+  );
 }
