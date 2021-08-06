75606fe1baad4e7d5a415c597c8dc1e617912846
Shaalan Marwan <salsanjary@gmail.com>
2021-08-06 21:12:31 +0300

    ✨ Incremental Static Regeneration for user posts

--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -1,8 +1,39 @@
+import { firestore, getUserWithUsername, postToJSON } from "../../lib/firebase";
 
 export default function PostPage() {
-    return (
-        <main>
-            
-        </main>
-    )
+  return <main></main>;
+}
+
+export async function getStaticProps({ params }: any) {
+  const { username, slug } = params;
+  const userDoc = await getUserWithUsername(username);
+  let post;
+  let path;
+  if (userDoc) {
+    const postRef = userDoc.ref.collection("posts").doc(slug);
+    post = postToJSON(await postRef.get());
+
+    path = postRef.path;
+
+    return {
+      props: {
+        post,
+        path,
+      },
+      revalidate: 5000,
+    };
+  }
+}
+export async function getStaticPaths() {
+  const snapshot = await firestore.collectionGroup("posts").get();
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data();
+    return {
+      params: { username, slug },
+    };
+  });
+  return {
+    paths,
+    fallback:'blocking'
+  };
 }
